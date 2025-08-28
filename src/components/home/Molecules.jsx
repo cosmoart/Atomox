c461841bcfc827c2ceb5b052e3b8f784173994a6
CosmoArt <cosmohydra17@gmail.com>
2025-08-27 21:37:49 -0500

    home

--- src/components/home/Molecules.jsx
+++ src/components/home/Molecules.jsx
@@ -1,7 +1,7 @@
 export default function Molecules () {
 	return (
 		<article className='mb-10'>
-			<img src='test.png' alt='test' className='w-full h-full object-cover' />
+			{/* <img src='test.png' alt='test' className='w-full h-full object-cover' /> */}
 		</article>
 	)
 }
